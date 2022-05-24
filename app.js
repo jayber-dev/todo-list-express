0730e20b9e6d9d89c5b858c232276c55995665ea
jay ber <jayber1@gmail.com>
2022-05-24 15:42:15 +0300

    user auth adding

--- app.js
+++ app.js
@@ -16,7 +16,10 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+
+}))
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/pics', express.static('public'))
