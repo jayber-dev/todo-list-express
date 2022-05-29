603cdcb304d4855c78ef069a427ac78e2d57871e
jay ber <jayber1@gmail.com>
2022-05-29 14:51:26 +0300

    ajax post priority kloging fixed

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,6 @@ routerIndex.post('/enter', function(req, res, next) {
             }
         })
         // console.log(dbData);
-
 })
 
 // ------------------------------ USER INTERFACE ---------------------------------
