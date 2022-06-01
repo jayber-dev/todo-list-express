eaf7f17a5cfbcbc5b81bee750b0ad97323da51d9
jay ber <jayber1@gmail.com>
2022-06-01 10:19:51 +0300

    working login

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,8 @@ const db = new sqlite3.Database('todo.db')
 // TODO: build SQL database
 // let varified = ""
 
+// ------------------------ LOGIN HANDLER ----------------------------------
+
 exports.validation = (req, res, next) => {
     req.user_logged = true
     console.log(req.body.email);
@@ -43,6 +45,8 @@ exports.validation = (req, res, next) => {
     }
 
 }
+
+// -------------------------- MAIN TASKS PAGE ------------------------------------------
 
 routerIndex.post('/interface', this.validation, function(req, res, next) {
     // console.log(req.params);
@@ -98,31 +102,6 @@ routerIndex.post('/addItem', (req, res, next) => {
     })
 })
 
-
-// ------------------------- REGISTER AND LOGIN HANDLING ----------------------------------
-
-// routerIndex.post('/', function(req, res, next) {
-//     console.log(req.session.id)
-//     res.sendStatus(200)
-//         // res.setHeader([req.session.cookie])
-//     const dbData = db.get(`SELECT id, email, hash_password From users WHERE email = ?`, [req.body.email], function(err, data) {
-//         if (err) { throw err }
-
-//         if (data == undefined) {
-//             res.render('login')
-//         } else if (req.body.pass == data.hash_password) {
-
-//             console.log(data);
-//             res.redirect('/tasks')
-//         } else {
-//             console.log("pass not match");
-//             res.redirect('/')
-//         }
-//     })
-//     res.redirect('/')
-//     console.log(dbData);
-
-// })
 
 // ----------------------------- REGISTRATION HANDLING ----------------------------------
 
