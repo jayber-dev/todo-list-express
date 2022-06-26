c09de2b96876554e3b060c77b96e7267fa0112d6
jay ber <jayber1@gmail.com>
2022-06-26 10:53:46 +0300

    omg dep

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,15 @@ const routerIndex = express.Router();
 let users = []
 
 
-routerIndex.use(session({ secret: `${process.env.SECRET_KEY}`, cookie: { maxAge: 10 * 900000 }, resave: false, saveUninitialized: false }));
+routerIndex.use(session({
+    name: "session",
+    keys: [process.env.SECRET_KEY],
+    maxAge: 24 * 60 * 60 * 1000,
+    // secret: `${process.env.SECRET_KEY}`,
+    // cookie: { maxAge: 10 * 900000 },
+    // resave: false,
+    // saveUninitialized: false
+}));
 
 // ------------------------ LOGIN HANDLER FUNCTION----------------------------------
 
